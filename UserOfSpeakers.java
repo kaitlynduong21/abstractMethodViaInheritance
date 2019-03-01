4b39254967e71fddd1d956d683be1c79abe7a5bf
Kaitlyn Duong <43048934+kaitlynduong21@users.noreply.github.com>
2019-03-01 13:37:44 -0500

    Update UserOfSpeakers.java

--- UserOfSpeakers.java
+++ UserOfSpeakers.java
@@ -2,7 +2,6 @@ public class UserOfSpeakers{
 
   public static void main(String[] args) {
 
-    Retriever doug = new Retriever();
     Animal pat = new Retriever();
     System.out.println(pat.speak());
   }
