1828e5982c291ba6a419e0d3b39bd574fd2f6699
kduong@stuy.edu <kduong@stuy.edu>
2019-02-28 19:12:56 -0500

    Added UserOf file

---
+++ UserOfSpeakers.java
@@ -0,0 +1,10 @@
+public class UserOfSpeakers{
+
+  public static void main(String[] args) {
+
+    Retriever doug = new Retriever();
+    Animal pat = new Retriever();
+    System.out.println(pat.speak());
+  }
+
+}
