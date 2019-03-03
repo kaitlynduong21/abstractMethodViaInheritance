f9da366080aa8679c7fde9320b779cb240a0f5df
Kaveen Sandagiripathir <ksandagiripathir@stuy.edu>
2019-03-03 12:21:35 -0500

    Kaveen's Edit

--- UserOfSpeakers.java
+++ UserOfSpeakers.java
@@ -1,11 +1,11 @@
 public class UserOfSpeakers{
 
   public static void main(String[] args) {
-
+    //Pat is of variable type Animal but object type Retriever. 
     Animal pat = new Retriever();
     System.out.println(pat.speak());
   }
 
 }
 
-//michael
+
